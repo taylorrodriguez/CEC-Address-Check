c5d6ab07097fc09727da821e64c26b27f3b2c176
Taylor Rodriguez <taylorerod@gmail.com>
2022-09-20 23:26:43 -0700

    optimize script

--- address-check.py
+++ address-check.py
@@ -1,81 +1,54 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 import pandas as pd
 import time
 from openpyxl import load_workbook
 
-# Chrome Driver Path
+# Get file name from user
+file_loc = input("Enter excel file name: ") + ".xlsx"
+
+# Chrome driver path
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+delay = 3
 
 # Site used to get address data
 driver.get("https://neighborhoodinfo.lacity.org/")
 
-# Get addresses from Excel File using Pandas
-file_loc = "Pynoos2 - cleaned.xlsx"
-df = pd.read_excel(file_loc, index_col = None, na_values = ['NA'], usecols = "A")
+# Store addresses from excel into a dataframe using pandas
+df = pd.read_excel(file_loc, index_col=None, na_values=['NA'], usecols="A")
 
-# Create blank arrays (lists) for in City, in District, and District Number
-cityList = []
-districtList = []
+# Create a blank array(list) for district numbers
 districtNumberList = []
-
-def inCityCheck():
-    # Find HTML element where the District information is located, by XPATH
-    districtNumber = driver.find_element(
-        By.XPATH, '/html/body/div[2]/div/div/div[3]/div[2]/div/main/section/div[2]/div/div/div[3]/table[1]/tr[5]/td[2]')
-    # Get Text from element, example: "District 13: Mitch O'Farrell", and convert to "CD 13"
-    districtNumberList.append(
-        "CD " + ((districtNumber.text).split(" ")[1])[:-1])
-    print(districtNumberList)
-    cityList.append("Y")
-    
-    if "Mitch O'Farrell" in driver.page_source:
-        time.sleep(1)
-        districtList.append("Y")
-        
-    else:
-        time.sleep(1)
-        districtList.append("N")
 
 # Iterate through addresses, search, and retrieve data
 for i, row in df.iterrows():
-    print(cityList, districtList)
-    time.sleep(2)
-    search = driver.find_element(
-        By.ID, 'ita-acsf-neighborhoodinfo-modal-field-address').clear()
-    search = driver.find_element(
-        By.ID, 'ita-acsf-neighborhoodinfo-modal-field-address')
-    search.send_keys(row)
-    search.send_keys(Keys.RETURN)
-    time.sleep(2)
-
-  # Decide if address is In City and In District according to whether Mayor and CD Official(ex. CD 13 Mitch O'Farrell) names are on the site.
-
-    if "Eric Garcetti" in driver.page_source:
         time.sleep(1)
-        inCityCheck()
+        search = driver.find_element(By.ID, 'ita-acsf-neighborhoodinfo-modal-field-address')
+        search.clear()
+        search.send_keys(row)
+        search.send_keys(Keys.RETURN)
+        time.sleep(3)
 
-    elif "Results Found:" in driver.page_source:
-        time.sleep(5)
-        inCityCheck()
+        try:
+            districtNumber = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[3]/div[2]/div/main/section/div[1]/div/div/div[3]/table[1]/tr[5]/td[2]/a')))
+            districtNumberList.append("CD " + ((districtNumber.text).split(" ")[1])[:-1])
+            print(districtNumberList)
+        except TimeoutException:
+            districtNumberList.append("None")
         
-    else:
-        time.sleep(1)
-        cityList.append("N")
-        time.sleep(1)
-        districtList.append("N")
-        districtNumberList.append("None")
-
 # Can check result in terminal
-print(cityList, districtList, districtNumberList)
+print(districtNumberList)
 
 # Post to Excel
 wb = load_workbook(file_loc)
 ws = wb.create_sheet("output")
 
-for row in zip(districtList, cityList, districtNumberList):
+for row in zip(districtNumberList):
     ws.append(row)
 
 wb.save(file_loc)
